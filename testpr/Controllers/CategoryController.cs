0b028d2517a23c0ac86766eb0c78ebf1c7f6ca8e
mahaamd <mohammadamiri.ma98@gmail.com>
2023-08-20 14:35:47 +0330

    Add project files.

---
+++ testpr/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using testpr.Data;
+using testpr.Models;
+
+namespace testpr.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+    
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.categories;
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ testpr/test.cs
@@ -0,0 +1,6 @@
+﻿namespace testpr
+{
+    public class test
+    {
+    }
+}
