99ba4472ebbf5baa40ef4e6918e4366428774b4f
mahaamd <mohammadamiri.ma98@gmail.com>
2023-08-21 16:25:47 +0330

    adding edit and delete part to app

--- testpr/Controllers/CategoryController.cs
+++ testpr/Controllers/CategoryController.cs
@@ -11,13 +11,13 @@ namespace testpr.Controllers
         {
             _db = db;
         }
-    
+
         public IActionResult Index()
         {
             IEnumerable<Category> objCategoryList = _db.categories;
             return View(objCategoryList);
         }
-        
+
         // GET
         public IActionResult Create()
         {
@@ -42,6 +42,76 @@ namespace testpr.Controllers
             }
             return View(obj);
         }
+
+        // GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            // TODO: do not foget to change this part
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("add custoom error", "the display cannot exactly be the same as name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            // TODO: do not foget to change this part
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var categoryFromDb = _db.categories.Find(id);
+            
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            _db.categories.Remove(categoryFromDb);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
     }
 
 }
+
--- testpr/Models/Category.cs
+++ testpr/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace testpr.Models
 {
@@ -8,6 +9,8 @@ namespace testpr.Models
         public int Id { get; set; }
         [Required] 
         public string Name { get; set; }
+        [DisplayName("display order")]
+        [Range(1, 100, ErrorMessage ="display order must be between 1 and 100")]
         public int DisplayOrder { get; set; }
         public DateTime CreateDateTime { get; set; } = DateTime.Now;
     }
